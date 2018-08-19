6ccb0af1ac1068b79dc2ccdfdcb382320b84c9f4
anhua <huabo1900@163.com>
2018-08-19 22:53:24 +0800

    Initial commit

---
+++ bobo-config/src/main/java/com/github/bobo/config/BoboConfigApplication.java
@@ -0,0 +1,12 @@
+package com.github.bobo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoboConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoboConfigApplication.class, args);
+    }
+}
---
+++ bobo-eureka/src/test/java/com/github/bobo/eureka/BoboEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.bobo.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoboEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
